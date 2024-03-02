d454d44a1fd03a48deb18ac795140d90f0398851
Ayaz-Ahamed-Shaik <ayaz55ahammad@gmail.com>
2024-03-02 20:02:01 +0530

    styling

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -7,7 +7,27 @@ function Register()
     return (
         <div className='HomeContainer'>
           <Header/>
-        <div className='RegisterContainer'></div>
+        <div className='registerContainer'>
+            <h2>Register Here</h2>
+            <div className='registerForm'>
+               <div className='option'>
+                <label>Name:</label>
+                  <input type='text'/>
+               </div>
+               <div className='option'>
+               <label>Email:</label>
+                  <input type='email'/>
+               </div>
+            <div className='option'>
+               <label>Password:</label>
+                  <input type='password'/>
+               </div>
+               <div className='option'>
+               <label>Confirm Password:</label>
+                  <input type='text'/>
+               </div>
+        </div>
+        </div>
           <Footer/>
         </div>
     );
